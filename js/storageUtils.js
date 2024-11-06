d6852ffa999c068da779693b3f00d8b5cba53d16
LukasDano <lukas.danowski@mail.de>
2024-11-06 16:29:40 +0100

    prettier formatting

--- js/storageUtils.js
+++ js/storageUtils.js
@@ -1,17 +1,17 @@
 function getCookie(name) {
-    const cookies = document.cookie.split(';');
+    const cookies = document.cookie.split(";");
     for (let cookie of cookies) {
-        const [cookieName, cookieValue] = cookie.trim().split('=');
+        const [cookieName, cookieValue] = cookie.trim().split("=");
         if (cookieName === name) {
             return decodeURIComponent(cookieValue);
         }
     }
 }
 
-function getBooleanCookie(name){
-    const cookies = document.cookie.split(';');
+function getBooleanCookie(name) {
+    const cookies = document.cookie.split(";");
     for (let cookie of cookies) {
-        const [cookieName, cookieValue] = cookie.trim().split('=');
+        const [cookieName, cookieValue] = cookie.trim().split("=");
         if (cookieName === name) {
             if (cookieValue === "true") {
                 return true;
@@ -22,21 +22,40 @@ function getBooleanCookie(name){
     }
 }
 
-function setCookie(name, value, expirationDate) {
-    let expires = '';
-    if (expirationDate instanceof Date) {
-        expires = '; expires=' + expirationDate.toUTCString();
+function getIntCookie(name) {
+    const cookies = document.cookie.split(";");
+    for (let cookie of cookies) {
+        const [cookieName, cookieValue] = cookie.trim().split("=");
+        if (cookieName === name) {
+            return parseInt(decodeURIComponent(cookieValue));
+        }
     }
-    document.cookie = name + '=' + encodeURIComponent(value) + expires + '; path=/';
+}
+
+function setCookie(name, value, expirationDate) {
+    let expires = "";
+    if (expirationDate instanceof Date) {
+        expires = "; expires=" + expirationDate.toUTCString();
+    }
+    document.cookie =
+        name + "=" + encodeURIComponent(value) + expires + "; path=/";
 }
 
 function setCookieUntilMidnight(name, value) {
     const now = new Date();
-    const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
-    const expires = '; expires=' + midnight.toUTCString();
-    document.cookie = name + '=' + encodeURIComponent(value) + expires + '; path=/';
+    const midnight = new Date(
+        now.getFullYear(),
+        now.getMonth(),
+        now.getDate() + 1,
+        0,
+        0,
+        0,
+    );
+    const expires = "; expires=" + midnight.toUTCString();
+    document.cookie =
+        name + "=" + encodeURIComponent(value) + expires + "; path=/";
 }
 
 function deleteCookie(name) {
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
