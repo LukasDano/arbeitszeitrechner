5abd5cb1a1d6e1492e45880ee999669a66cf443f
LukasDano <142010573+LukasDano@users.noreply.github.com>
2025-07-24 07:25:36 +0200

    overTimeIcon loads on intial Page loading

--- js/custom/dzr.js
+++ js/custom/dzr.js
@@ -455,6 +455,14 @@ $(document).ready(() => {
         }
     }
 
+    function setOverTimeAutomaticIcon() {
+        const iconFileName = getBooleanCookie("overTimeAutomaticActive") ? "automaticOn" : "automaticOff";
+        const overTimeAutomaticIconImg = document.getElementById("overTimeAutomaticIcon");
+        
+        overTimeAutomaticIconImg.src = "pictures/icons/" + iconFileName + ".png";
+        overTimeAutomaticIconImg.alt = iconFileName;
+    }
+
     if (getCookie("windowInitLoaded") && getCookie("start") != null) {
         readSollAnPauseFromLocalStorageAndSetInFields();
         readStartAndFloatFromLocalStorageAndSetInFields();
@@ -465,11 +473,12 @@ $(document).ready(() => {
         setCountdown();
         optimizeEnd();
         updateEndTimeAfterWorkIsOver();
-        setOverTimeIconToCookieValue();
+        setOverTimeAutomaticIcon();
     } else {
         resetCookies();
         setCookieUntilMidnight("modus", "7h06m");
         setCookieUntilMidnight("windowInitLoaded", "true");
+        setOverTimeAutomaticIcon();
         $("#start").focus();
     }
 
@@ -512,6 +521,9 @@ $(document).ready(() => {
         switchModeIfIsAllowed();
     }
 
+    /**
+    * @Deprecated stattdessen wenn möglich lieber setOverTimeAutomaticIcon() benutzen (namen vlt. noch tauschen)
+    */
     function setOverTimeIconToCookieValue() {
         const overTimeAutomaticActive = getBooleanCookie("overTimeAutomaticActive");
 
